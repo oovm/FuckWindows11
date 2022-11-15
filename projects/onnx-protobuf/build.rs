0035a3115004ef77a729dd461ffcb1af3a8cc200
Aster <galaster@foxmail.com>
2022-11-16 03:39:16 +0800

    🔥 Remove build script

--- projects/onnx-protobuf/build.rs
+++
@@ -1,3 +0,0 @@
-fn main() {
-    prost_build::compile_protos(&["onnx/onnx.proto3"], &["third_party/onnx"]).unwrap();
-}
--- projects/safe-tensors-load/tests/main.rs
+++ projects/safe-tensors-load/tests/main.rs
@@ -2,3 +2,9 @@
 fn ready() {
     println!("it works!")
 }
+
+#[test]
+fn main() {
+    prost_build::compile_protos(&["../onnx/onnx/onnx.proto3"], &["../onnx/onnx"]).unwrap();
+    std::fs::copy("foo.txt", "bar.txt")?;
+}
